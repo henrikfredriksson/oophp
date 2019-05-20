7ad3261103a62b0eba1b8540c2b1683e71d028ae
Henrik Fredriksson <henrikfredriksson2@gmail.com>
2019-05-20 14:06:12 +0200

    do not allow direct access to play router

--- view/dice/play.php
+++ view/dice/play.php
@@ -95,104 +95,112 @@ require __DIR__ . "/toRGB.php";
 <div id="container">
     <div>
         <h1>Tärningspelet 100</h1>
-        <?php if ($players[0]->getScore() + $players[1]->getScore() > 0) : ?>
-        <h2>Nuvarande ställning</h2>
-            <div id="currentScores">
-                <ul>
-                    <li>
-                    <div class="scoreBar">
-                            <div
-                                class="score"
-                                style=
-                                    "background-color: <?= toRGB($players[0]->getScore()) ?>;
-                                    width: <?= 5*$players[0]->getScore()?>px">
-                            <strong><?= $players[0]->getScore() <= 100 ? $players[0]->getScore() : "100" ?></strong>
-                            </div>
-                    </div>
-                    Spelare
-                    </li>
-                    <li>
+        <?php if (!$players) : ?>
+            <h2>Starta ett nytt spel</h2>
+            <p>Det ser ut som du försöker nå spelsidan utan att ha startat ett nytt spel</p>
+            <input type="submit" name="doInit" value="Nytt spel">
+
+        <?php else : ?>
+            <?php if ($players[0]->getScore() + $players[1]->getScore() > 0) : ?>
+            <h2>Nuvarande ställning</h2>
+                <div id="currentScores">
+                    <ul>
+                        <li>
                         <div class="scoreBar">
-                            <div class="score"
-                                style=
-                                    "background-color: <?= toRGB($players[1]->getScore()) ?>;
-                                    width: <?= 5*$players[1]->getScore() ?>px">
-                        <strong><?= $players[1]->getScore() <= 100 ? $players[1]->getScore() : "100" ?></strong>
-                            </div>
+                                <div
+                                    class="score"
+                                    style=
+                                        "background-color: <?= toRGB($players[0]->getScore()) ?>;
+                                        width: <?= 5*$players[0]->getScore()?>px">
+                                <strong><?= $players[0]->getScore() <= 100 ? $players[0]->getScore() : "100" ?></strong>
+                                </div>
                         </div>
-                        Marvin
-                    </li>
-                </ul>
+                        Spelare
+                        </li>
+                        <li>
+                            <div class="scoreBar">
+                                <div class="score"
+                                    style=
+                                        "background-color: <?= toRGB($players[1]->getScore()) ?>;
+                                        width: <?= 5*$players[1]->getScore() ?>px">
+                            <strong><?= $players[1]->getScore() <= 100 ? $players[1]->getScore() : "100" ?></strong>
+                                </div>
+                            </div>
+                            Marvin
+                        </li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+
+
+
+            <?php if ($players[0]->getScore() >= 100) : ?>
+                <h1>Grattis, du vann över Marvin! </h1>
+                <form method="POST">
+                    <input type="submit" name="doInit" value="Nytt spel">
+                </form>
+
+            <?php elseif ($players[1]->getScore() >= 100) : ?>
+                <h1>Tvärr, Marvin vann! </h1>
+                <form method="POST">
+                    <input type="submit" name="doInit" value="Nytt spel">
+                </form>
+
+            <?php else : ?>
+            <h2>Nu spelar <?= $currentPlayer == 0 ? "du" : "Marvin" ?></h2>
+
             </div>
+
+            <p>Poäng i potten: <?= $currentSum ?> </p>
+
+
+            <div class="dices">
+
+                <?php if ($graphics && $sum) : ?>
+                    <p class="dice-sprite">
+                        <?php foreach ($graphics as $value) : ?>
+                            <i class="dice-sprite <?= $value ?>"></i>
+                        <?php endforeach; ?>
+                    </p>
+                <?php endif; ?>
+
+
+            </div>
+            <div id="form">
+            <form method="POST">
+
+            </div>
+            <div id="buttons">
+                <input id="roll" type="submit" name="doRoll" value=
+                <?php
+
+                if (!$currentPlayer) {
+                    echo "Kasta tärningar";
+                } else {
+                    echo "Simulera Marvin";
+                }
+
+                ?>
+                >
+
+                <?php if ($currentPlayer == 0) : ?>
+                <input id="save" type="submit" name="doSave" value="Spara"
+                    <?php
+                    if (!$continue || $currentPlayer == 1) {
+                        echo "disabled";
+                    }
+                    ?>
+                >
+                <?php endif; ?>
+
+                <input type="submit" name="doInit" value="Nytt spel">
+
+            </form>
+
+            <?php endif; ?>
         <?php endif; ?>
-
-
-
-    <?php if ($players[0]->getScore() >= 100) : ?>
-        <h1>Grattis, du vann över Marvin! </h1>
-        <form method="POST">
-            <input type="submit" name="doInit" value="Nytt spel">
-        </form>
-
-    <?php elseif ($players[1]->getScore() >= 100) : ?>
-        <h1>Tvärr, Marvin vann! </h1>
-        <form method="POST">
-            <input type="submit" name="doInit" value="Nytt spel">
-        </form>
-
-    <?php else : ?>
-    <h2>Nu spelar <?= $currentPlayer == 0 ? "du" : "Marvin" ?></h2>
-
     </div>
-
-    <p>Poäng i potten: <?= $currentSum ?> </p>
-
-
-    <div class="dices">
-
-        <?php if ($graphics && $sum) : ?>
-            <p class="dice-sprite">
-                <?php foreach ($graphics as $value) : ?>
-                    <i class="dice-sprite <?= $value ?>"></i>
-                <?php endforeach; ?>
-            </p>
-        <?php endif; ?>
-
-
-    </div>
-    <div id="form">
-    <form method="POST">
-
-    </div>
-    <div id="buttons">
-        <input id="roll" type="submit" name="doRoll" value=
-        <?php
-
-        if (!$currentPlayer) {
-            echo "Kasta tärningar";
-        } else {
-            echo "Simulera Marvin";
-        }
-
-        ?>
-        >
-
-        <?php if ($currentPlayer == 0) : ?>
-        <input id="save" type="submit" name="doSave" value="Spara"
-            <?php
-            if (!$continue || $currentPlayer == 1) {
-                echo "disabled";
-            }
-            ?>
-        >
-        <?php endif; ?>
-
-        <input type="submit" name="doInit" value="Nytt spel">
-
-    </form>
-    </div>
-    <?php endif; ?>
-
 </div>
 
 
